2658761d9fd47b99054058b275a7446ec81ac5a9
Vincent Bonamy <vbonamy@gmail.com>
2024-03-26 14:08:18 +0100

    Suppression totale de esupsgcclient-evolis en faveur de esupsgcclient-evolis-sdk

--- esupsgcclient-evolis/src/main/java/org/esupportail/esupsgcclient/service/printer/evolis/EvolisSocketException.java
+++
@@ -1,7 +0,0 @@
-package org.esupportail.esupsgcclient.service.printer.evolis;
-
-public class EvolisSocketException extends Exception {
-    public EvolisSocketException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
