e57233391ffe3f9fd509882670cb94b222c81313
Vincent Bonamy <vbonamy@gmail.com>
2026-08-24 11:05:41 +0200

    Debug montée de version jackson

--- esupsgcclient-core/src/main/java/org/esupportail/esupsgcclient/service/pcsc/EsupNfcTagRestClientService.java
+++ esupsgcclient-core/src/main/java/org/esupportail/esupsgcclient/service/pcsc/EsupNfcTagRestClientService.java
@@ -12,7 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @Component
